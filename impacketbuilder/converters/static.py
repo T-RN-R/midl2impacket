3bdddbe4ec218d514d929e2ff7d880d07ce8567b
eMgTy <mitchellt626@gmail.com>
2021-08-10 13:33:11 -0400

    Fixed bug where multiple sets of Python imports occured

--- impacketbuilder/converters/static.py
+++ impacketbuilder/converters/static.py
@@ -1,16 +1,20 @@
-import pathlib
-from midlparser import parse_idl
-from impacketbuilder.converters.typing import IDL_TYPES
-from midl import MidlImport
-from .base import *
+from .base import Converter
+from .struct import MidlStructConverter
+from .procedure import MidlProcedureConverter
+from midl import *
 from .typing import *
+from .comments import MidlCommentWriter
 
-class MidlImportsConverter(Converter):
-    def convert(self, imports:list[MidlImport], import_dir:str, def_converter):
-        #TODO add imports here
-        for _import in imports:
-            in_file = pathlib.Path(import_dir+_import.file.replace("\"",""))
-            def_converter.convert(parse_idl(in_file), import_dir)
+
+class MidlStaticConverter(Converter):
+    HAS_RUN = False
+    def convert(self, imports:list[MidlImport]):
+        if not __class__.HAS_RUN:
+            comment_writer = MidlCommentWriter(self.io, self.tab_level)
+            comment_writer.comment_block("Generated from MIDL2Impacket.py")
+            self.base_imports()
+            self.type_mapping()
+            __class__.HAS_RUN = True
 
     def base_imports(self):
         imports = """
