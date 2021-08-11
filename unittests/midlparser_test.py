9c27624e96d2a28838af04b9abb5c47a94c9fce7
knifeyspoony <knspoony@gmail.com>
2021-08-11 14:32:06 -0400

    meh

--- unittests/midlparser_test.py
+++ unittests/midlparser_test.py
@@ -6,8 +6,8 @@ def test_full():
     for scraped_file in scraped_files:
         print(f"Parsing IDL {scraped_file}")
         midl = parse_idl(scraped_file)
+        print(f"Successfully parsed IDL {scraped_file}")
 
 
-
-        
-
+if __name__ == "__main__":
+    test_full()
